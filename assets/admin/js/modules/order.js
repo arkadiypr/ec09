b9cc03ff045173a0fd46b5ce0c63499779a962b2
arkadiypr <arkadij.pryadko@gmail.com>
2019-10-22 15:04:29 +0300

    Админка для заказов. Обносление цены товара и общей суммы заказа при извенении товара в списке

--- assets/admin/js/modules/order.js
+++ assets/admin/js/modules/order.js
@@ -8,21 +8,41 @@ window.addEventListener('load', () => {
 
 	if (container) {
 		container.addEventListener('input', (event) => {
-			if (!event.target.classList.contains('js-update-amount')) {
-				return;
+			if (event.target.classList.contains('js-update-amount')) {
+				updateAmount();
 			}
 
-			let amount = 0;
-			let rows = container.querySelectorAll('.sonata-ba-tbody tr');
+		});
 
-			rows.forEach((row) => {
-				let priceElement = row.querySelector('[name*=price]');
-				let countElement = row.querySelector('[name*=count]');
+		$(container).on('change', (event) => {
+			if (event.target.name.indexOf(['product']) > 0) {
+				updatePrice(event.target);
+			}
+		});
+	}
 
-				amount += priceElement.value * countElement.value;
-			});
+	function updateAmount() {
+		let amount = 0;
+		let rows = container.querySelectorAll('.sonata-ba-tbody tr');
 
-			amountElement.value = amount;
-		})
+		rows.forEach((row) => {
+			let priceElement = row.querySelector('[name*=price]');
+			let countElement = row.querySelector('[name*=count]');
+
+			amount += priceElement.value * countElement.value;
+		});
+
+		amountElement.value = amount;
+	}
+	
+	function updatePrice(select) {
+		let option = select.selectedOptions[0];
+		let row = select.closest('tr');
+
+		let priceElement = row.querySelector('[name*=price]');
+
+		priceElement.value = option.dataset.price;
+		updateAmount();
+
 	}
 });
